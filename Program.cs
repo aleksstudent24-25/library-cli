c50fb117c8cb3b7130e1a6d6ca0f93087092853e
the-bug-lover <aleksander24sfj@gmail.com>
2024-11-12 10:31:34 +0100

    setup user input parsing

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+bool runProgram = true;
+while (runProgram)
+{
+    // Read user input
+    Console.WriteLine("Commands available: lend, return & exit");
+    string userInput = Console.ReadLine()
+        ?? throw new ArgumentNullException("Your input came back as null");
+
+    // Loaning the book
+    if (userInput == "lend")
+    {
+        Console.WriteLine("Lending a book");
+    }
+
+    // Returning the book
+    else if (userInput == "return")
+    {
+        Console.WriteLine("Returning a book");
+    }
+
+    // Exit program
+    else if (userInput == "exit")
+    {
+        runProgram = false;
+    }
+}
